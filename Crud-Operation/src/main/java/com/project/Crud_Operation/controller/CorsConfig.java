d0f3d93fd409210b88212fff9218af7b7acca98c
Vignesh S <152682153+SVignesh2004@users.noreply.github.com>
2024-12-08 21:44:38 +0530

    Add files via upload

---
+++ Crud-Operation/src/main/java/com/project/Crud_Operation/controller/CorsConfig.java
@@ -0,0 +1,20 @@
+package com.project.Crud_Operation.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RestController
+@CrossOrigin("http://localhost:5173")
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+    }
+}
